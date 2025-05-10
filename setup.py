018b6c5813ce328697b8fe9fad2af95037a6268b
RoushanRaj-cmd <roushanraj6641@gmail.com>
2025-05-10 18:31:20 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+'''Responsible for creating my machine learning project as a package'''
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return a list of requiremnts
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Roushan',
+    author_email='roushanraj6641@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
+
+#-e . in requirements.txt indicates that there is a setup.py file and metadata needs to be generated.
