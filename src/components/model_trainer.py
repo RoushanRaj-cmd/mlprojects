ed73a313e9766e20bfa6b441a26d475825af38ea
RoushanRaj-cmd <roushanraj6641@gmail.com>
2025-05-14 11:29:03 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,15 +42,41 @@ class ModelTrainer:
             models={
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
-                "Decision True":DecisionTreeRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
                 "Random Forest Regressor":RandomForestRegressor(),
                 "XGBRegressor":XGBRegressor(),
                 "CatBoosting Regressor":CatBoostRegressor(verbose=False),
                 "Adaboost Regressor":AdaBoostRegressor()
             }
 
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest Regressor":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                },
+                "XGBRegressor":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "Adaboost Regressor":{
+                    'learning_rate':[0.1,0.01,0.5,0.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+
             model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=params)
             
             best_model_score=max(sorted(model_report.values()))
             
